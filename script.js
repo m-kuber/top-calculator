82280c32ed79a606189c9120a919fe8f33a1133b
Kuber <kishore.kuber23@vit.edu>
2024-10-01 09:28:56 +0530

    Add functions for functionality of buttons
    The functions for the mathematical operators, the clear and backspace
    buttons, and for taking the input from the keyboard have been added.

--- script.js
+++ script.js
@@ -15,33 +15,55 @@ function multiply(a, b)
 
 function divide(a, b)
 {
-    return parseInt(a) / parseInt(b);
+    return Math.round((parseInt(a) / parseInt(b)) * 100) / 100;
+}
+
+const operate = (operator, a, b) => {
+    if (operator === '+')
+    {
+        return add(a, b);
+    }
+    else if (operator === '-')
+    {
+        return subtract(a, b);
+    }
+    else if (operator === '*')
+    {
+        return multiply(a, b);
+    }
+    else if (operator === '/')
+    {
+        return divide(a, b);
+    }
+    else
+    {
+        return "INVALID!";
+    }
 }
 
 const result = document.querySelector("#result");
 const operation = document.querySelector("#operation");
 
-let operands = [0, 0];
+let num1 = 0, num2 = 0;
 let operator = '';
 
-const zero = document.querySelector("#zero");
-zero.addEventListener('click', () => {
-    result.textContent += "0";
+const clear = document.querySelector("#clear");
+clear.addEventListener('click', () => {
+    result.textContent = "";
+    num1 = 0;
+    num2 = 0;
+    operator = '';
 });
 
-const plus = document.querySelector("#plus");
-plus.addEventListener('click', () => {
-    operator = '+';
-    result.textContent += "+";
-});
+const backspace = document.querySelector("#backspace");
+backspace.addEventListener('click', () => {
+    let length = result.textContent.length;
+    result.textContent = result.textContent.slice(-(length), -1);
+})
 
-const one = document.querySelector("#one");
-one.addEventListener('click', () => {
-    result.textContent += "1";
-});
-
-const equals = document.querySelector("#equals-button");
-equals.addEventListener('click', () => {
-    operation.textContent = result.textContent + " ="
-    result.textContent = "Hello world";
+document.addEventListener('keydown', (event) => {
+    if ((event.key >= '0' && event.key <= '9') || (['+', '-', '*', '/'].includes(event.key)))
+    {
+        result.textContent += `${event.key}`;
+    }
 });
